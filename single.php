37d444258b95c19c4bb7acc5c88149694f353fbd
emiliahenriksson <emiliahenriksson99@gmail.com>
2021-05-13 16:01:11 +0200

    delete comments and remove from saved

--- single.php
+++ single.php
@@ -15,10 +15,28 @@ session_start();
 <body>
     
 <?php
+
 $user = $_SESSION["username"];
+$userId = $_SESSION["userId"];
 
 if ($_SESSION["username"] != "") {
     echo "You are logged in as $user";
+
+    /**
+     * Save recipe
+     */
+    echo "<form action='' method='POST'>";
+        echo "<input type='submit' name='save' value='save'>";
+    echo "</form>";
+
+    /**
+     * Comments
+     */
+    echo "<form action='' method='POST'>";
+        echo "<input type='text' name='comment' id='comment' placeholder='comment'>";
+        echo "<input type='submit' name='submit' value='submit'>";
+    echo "</form>";
+
 } else {
     echo "You are not logged in";
 }
@@ -26,9 +44,40 @@ if ($_SESSION["username"] != "") {
 ?>
 
 
-<form action="" method=POST>
-    <input type="submit"name="save" value="save">
-</form>
+<?php
+
+if ($_GET['id']) {
+    $recipeId = $_GET['id'];
+} else if ($_GET['ida']) {
+    $recipeId = $_GET['ida'];
+}
+
+?>
+
+
+<?php
+
+if (isset($_POST['submit'])) {
+    if ($_POST['comment'] != "") {
+        echo "working";
+
+        $userId = $_SESSION["userId"];
+        $comment = $_POST['comment'];
+
+        if ($_GET['id']) {
+            $query = "INSERT INTO `comments` ( authorId, recipeId, comment ) VALUES ( '$userId', '$recipeId', '$comment' )";
+            mysqli_query($db, $query);
+
+        } else if ($_GET['ida']) {
+            $query = "INSERT INTO `comments` ( authorId, recipeIdAPI, comment ) VALUES ( '$userId', '$recipeId', '$comment' )";
+            mysqli_query($db, $query);
+        }
+    }
+}
+
+?>
+
+
 
 <?php
 
@@ -38,53 +87,32 @@ if (isset($_POST['save'])) {
 
     $query = "SELECT * FROM `userRecipes` WHERE `userId` = '$userId'";
 
-    //echo $search;
-
     $stmt = $db->prepare($query);
     $stmt->bind_result($uId, $rId, $raId);
     $stmt->execute();
 
     while($stmt->fetch()) {
 
-
-        echo "<br> raId: $raId <br>";
-        echo $_GET['ida'];
-
-
         if ($_GET['id']) {
-            $recipeId = $_GET['id'];
 
-            if ($uId == $userId && $rId != $recipeId) {
-                if ($rId == $recipeId):
+            if ($uId == $userId && $rId == $recipeId) {
                     $duplicate = "false";
-                endif;
-                echo "<br> db";
             }
-            
 
         } else if ($_GET['ida']) {
-            $recipeId = $_GET['ida'];
-
-            if ($uId == $userId && $raId != $recipeId) {
-                if ($raId == $recipeId):
-                    $duplicate = "false";
-                endif;
-                echo "<br> api $duplicate";
-            }
-
+            if ($uId == $userId && $raId == $recipeId): 
+                $duplicate = "false";
+            endif;
         }
-
     }
 
     $stmt->close();
 
-    echo "it is $duplicate";
-
-    if ($_GET['id'] && $duplicate == "false") {
+    if ($_GET['id'] && $duplicate != "false") {
         $query = "INSERT INTO `UserRecipes` (userId, recipeId) VALUES ('$userId', '$recipeId')";
         mysqli_query($db, $query);
     }
-    else if ($_GET['ida'] && $duplicate == "false") {
+    else if ($_GET['ida'] && $duplicate != "false") {
         $query = "INSERT INTO `UserRecipes` (userId, recipeIdAPI) VALUES ('$userId', '$recipeId')";
         mysqli_query($db, $query);
     }
@@ -97,13 +125,45 @@ if (isset($_POST['save'])) {
 
 
 
+<?php
+
+if ($_GET['id']) {
+    $query = "SELECT commentId, authorId, recipeId, comment FROM `comments` WHERE recipeId = $recipeId";
+} else if ($_GET['ida']) {
+    $query = "SELECT commentId, authorId, recipeIdAPI, comment FROM `comments` WHERE recipeIdAPI = $recipeId";
+}
+
+$stmt = $db->prepare($query);
+$stmt->bind_result($commentId, $authorId, $recipeId, $comment);
+$stmt->execute();
+
+while($stmt->fetch()) {
+    echo "<br><br>$comment";
+
+    $link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]?delete=delete";
+    echo $link;
+
+    if ($authorId == $userId) {
+        echo "<form action='includes/deleteComment.php' method='POST'>";
+            echo "<input type='hidden' name='commentId' value='$commentId'>";
+            echo "<input type='submit' value='delete' name='delete'>";
+        echo "</form>";
+    }
+
+}
+
+//$stmt-close();
+
+
+
+?>
+
+
 
 
 <?php
 
 if ($_GET['id']) {
-
-    $recipeId = $_GET['id'];
 
     $query = "SELECT * FROM `recipes` JOIN `user` ON recipes.authorId = user.userId WHERE `recipeId` = $recipeId";
 
@@ -139,64 +199,68 @@ if ($_GET['id']) {
 
 if ($_GET['ida']) {
 
-    $recipeId = $_GET['ida'];
-
     $curl = curl_init();
 
-        curl_setopt_array($curl, [
-            CURLOPT_URL => "https://tasty.p.rapidapi.com/recipes/detail?id=$recipeId",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => [
-                "x-rapidapi-host: tasty.p.rapidapi.com",
-                "x-rapidapi-key: 7f93a79ef4mshff235a3dc02cacap146162jsn4849d7364788"
-            ],
-        ]);
+    curl_setopt_array($curl, [
+        CURLOPT_URL => "https://tasty.p.rapidapi.com/recipes/detail?id=$recipeId",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => [
+            "x-rapidapi-host: tasty.p.rapidapi.com",
+            "x-rapidapi-key: 7f93a79ef4mshff235a3dc02cacap146162jsn4849d7364788"
+        ],
+    ]);
 
-        $response = curl_exec($curl);
-        $jsonArrayResponse = json_decode($response, true);
-        $err = curl_error($curl);
+    $response = curl_exec($curl);
+    $jsonArrayResponse = json_decode($response, true);
+    $err = curl_error($curl);
 
-        curl_close($curl);
+    curl_close($curl);
 
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
 
-            //image
-            $imgName = $jsonArrayResponse[thumbnail_url];
-            echo "<img src='$imgName' >";
+        //image
+        $imgName = $jsonArrayResponse[thumbnail_url];
+        echo "<img src='$imgName' >";
 
-            //instructions
-            foreach ($jsonArrayResponse[instructions] as $i) {
-                echo $i[display_text];
-                echo '<br>';
+        //instructions
+        foreach ($jsonArrayResponse[instructions] as $i) {
+            echo $i[display_text];
+            echo '<br>';
+        }
+
+        //title
+        $recipeTitle = $jsonArrayResponse[name];
+        echo $recipeTitle;
+
+        //ingredients
+        foreach($jsonArrayResponse[sections] as $i) {
+            foreach($i[components] as $a) {
+                echo $a[raw_text];
+                echo "<br>";
             }
-
-            //title
-            $recipeTitle = $jsonArrayResponse[name];
-            echo $recipeTitle;
-
-            //ingredients
-            foreach($jsonArrayResponse[sections] as $i) {
-                foreach($i[components] as $a) {
-                    echo $a[raw_text];
-                    echo "<br>";
-                }
-            }
-        
-        }  
+        }
+    }  
 }
 
 ?>
 
+<?php
 
+if (isset($_DELETE['delete'])) {
+    header("LOCATION: http://www.google.com");
+    echo "<br><br>hello";
+    $query = "DELETE FROM `comments` WHERE commentId = $commentId";
+}
 
+?>
 
 
 </body>
